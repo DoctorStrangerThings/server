7c5efb9f88888919076d7e20a028ab15c10a965f
DoctorStrangerThings <christian.soliva@wvsu.edu.ph>
2025-07-25 15:51:01 +0800

    added route

--- index.js
+++ index.js
@@ -15,6 +15,10 @@ const app = express();
 const port = process.env.PORT || 5000;
 const GEOCODE_API = process.env.OPENCAGE_API_KEY;
 
+app.get("/", (req, res) => {
+  res.send("API is running.");
+});
+
 // 🌐 Get local IP
 function getLocalIP() {
   const interfaces = os.networkInterfaces();
