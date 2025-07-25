712959db60be026d557374015d9b59eed2d6ba35
DoctorStrangerThings <142270844+DoctorStrangerThings@users.noreply.github.com>
2025-07-25 08:02:15 +0000

    added cors

--- index.js
+++ index.js
@@ -15,6 +15,9 @@ const app = express();
 const port = process.env.PORT || 5000;
 const GEOCODE_API = process.env.OPENCAGE_API_KEY;
 
+app.use(cors());
+
+
 app.get("/", (req, res) => {
   res.send("API is running.");
 });
